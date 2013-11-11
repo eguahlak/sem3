2854e166ac823fb60401679732465248be262628
Thomas <Thomas@10.18.138.79>
2013-11-11 11:02:07 +0100

    Anders commit

--- SmallJavaApp/src/smalljavaapp/SmallJavaApp.java
+++ SmallJavaApp/src/smalljavaapp/SmallJavaApp.java
@@ -17,7 +17,7 @@ public class SmallJavaApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Git");
+        System.out.println("Hello Git, killroy");
     }
     
 }
